3472eb0ea6f918051505b172f8a6a036ffbbea23
Santos Pierre <santospierreemmanuel@gmail.com>
2019-08-27 00:04:18 +0200

    Creating Very Simple API

--- node-farm/starter/index.js
+++ node-farm/starter/index.js
@@ -1,10 +1,13 @@
-// const fs = require('fs');
+const fs = require('fs');
 const http = require('http');
 const url = require("url");
 
 
 ////////////////////////
 /////SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
 /**
  * Function that create a server, the callback function in parameter will be called each time the server make a request
  * @req request Which request was call
@@ -17,6 +20,11 @@ const server = http.createServer((req,res)=>{
     res.end("This is the overview!");
   }else if(pathName === '/products'){
     res.end('This is the product');
+  }else if(pathName === '/api'){
+      res.writeHead(200, {
+        "Content-type": "application/json"
+      });
+      res.end(data);
   }else{
     //Putting Error, putting headers to inform the browser,client about the response
     res.writeHead(404,{
@@ -25,8 +33,6 @@ const server = http.createServer((req,res)=>{
     })
     res.end("<h1>Page not found</h1>");
   }
-
-  res.end('Hello from the server!');
 });
 /**
  * Listen method (Port,Host(localhost/127.0.0.1 by default), callback)
